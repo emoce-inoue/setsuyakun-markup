8e3e158c228bedf47e0e3388b963d036a6e32e80
inoue <emocedigital@gmail.com>
2024-10-08 16:48:29 +0900

    フォーマット修正

--- src/js/common.js
+++ src/js/common.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
   averageAnnualPremiumElem.textContent = averageAnnualPremiumValue.toLocaleString();
   annualPremiumElem.textContent = annualPremiumValue.toLocaleString();
   // 各カウントアップ要素に対応する「data-count-to」を設定
-  countUpElems.forEach(targetElem => {
+  countUpElems.forEach((targetElem) => {
     if (targetElem.classList.contains('js-saving-amount')) {
       targetElem.setAttribute('data-count-to', savingAmount);
     } else if (targetElem.classList.contains('js-monthly-saving-amount')) {
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const startTime = performance.now();
     const countUp = () => {
       const elapsed = performance.now() - startTime;
-      const countValue = from + ((elapsed / duration) * (to - from));
+      const countValue = from + (elapsed / duration) * (to - from);
       if (elapsed >= duration) {
         targetElem.innerText = to.toLocaleString();
       } else {
@@ -47,8 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
     requestAnimationFrame(countUp);
   };
 
-  const countUpObserver = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
+  const countUpObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
       if (entry.isIntersecting) {
         const targetElem = entry.target;
         const to = parseInt(targetElem.getAttribute('data-count-to'), 10);
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  countUpElems.forEach(targetElem => {
+  countUpElems.forEach((targetElem) => {
     countUpObserver.observe(targetElem);
   });
 });
