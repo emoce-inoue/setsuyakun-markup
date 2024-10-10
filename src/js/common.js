5106b766427fe78617de06b71f2b9b87303a9b67
inoue <emocedigital@gmail.com>
2024-10-10 16:07:23 +0900

    ヘッダーのマークアップ、アニメーション修正

--- src/js/common.js
+++ src/js/common.js
@@ -62,4 +62,21 @@ document.addEventListener('DOMContentLoaded', () => {
   countUpElems.forEach((targetElem) => {
     countUpObserver.observe(targetElem);
   });
+
+  const hamburger = document.querySelector('.js-hamburger');
+  const drawer = document.getElementById('js-drawer');
+  const drawerCloseButton = document.querySelector('.js-drawer-close');
+  const showDrawer = () => {
+    if (drawer.open) {
+      drawer.close();
+    }
+    drawer.setAttribute('data-active', 'true');
+    drawer.showModal();
+  };
+  const hideDrawer = () => {
+    drawer.setAttribute('data-active', 'false');
+    drawer.close();
+  };
+  hamburger.addEventListener('click', showDrawer);
+  drawerCloseButton.addEventListener('click', hideDrawer);
 });
